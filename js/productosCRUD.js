fd197d8c844687b86fa1d427a8920297d9078ef7
Pablo Gustavo Valenti <valenti.pablo@gmail.com>
2023-07-01 21:24:03 -0300

    Validación campos

--- js/productosCRUD.js
+++ js/productosCRUD.js
@@ -1,12 +1,35 @@
+// ===================================================== //
+// Función: vacio                                        //
+// Descripción: Determina si valor es vacío o no         //
+// Parámetros:                                           //
+//      campo = valor del campo                          //
+// Retorno:                                              //
+//      ret = TRUE, está vacío                           //
+//           FALSE, no está vacío                        //
+// ===================================================== //
+function vacio(campo) {
+    // Saco espacios y me aseguro que tenga valor
+    campo = (campo == undefined) ? "" : campo.trim();
+  
+    return (campo.length === 0);
+}
+
+// Creo la aplicación VUE
 const { createApp } = Vue
 createApp({
     data() {
         return {
+            // Lista de productos
             productos: [],
-            url: 'https://grupo18.pythonanywhere.com/productos', // si ya lo subieron a pythonanywhere
+            // URL para obtener los productos
+            url: 'https://grupo18.pythonanywhere.com/productos', 
+            // Indico error
             error: false,
+            // Indico operación exitosa
+            ok: false,
+            // Indico cargando
             cargando: true,
-            /*atributos para  guardar los valores del formulario */
+            // Campos del formulario
             id: 0,
             nombre: "",
             precio: 0,
@@ -18,13 +41,26 @@ createApp({
             imagen2: "",
             imagen3: "",
             imagen4: "",
+            // Error en los campos
+            errorNombre: false,
+            errorPrecio: false,
+            errorFranquicia: false,
+            errorTamano: false,
+            errorMaterial: false,
+            errorAltura: false,
+            errorImagen1: false,
+            errorImagen2: false,
+            errorImagen3: false,
+            errorImagen4: false,
         }
     },
     methods: {
         fetchData(url) {
+            // Obtengo los datos de todos los productos
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
+                    // Guardo los productos
                     this.productos = data;
                     this.cargando = false
                 })
@@ -34,48 +70,78 @@ createApp({
                 })
         },
         eliminar(producto) {
+            // Pregunto si se quiere elimninar el producto
             if (confirm('¿Estás seguro de que deseas eliminar este producto?'))
             {
+                // Armo la url para eliminar el producto
                 const url = this.url + '/' + producto;
+                // Preparo el comando
                 var options = {
                     method: 'DELETE',
                 }
+
+                // Envio el comando
                 fetch(url, options)
-                    .then(res => res.text()) // or res.json()
                     .then(res => {
-                        location.reload();
-                    })    
+                        res.text()
+                        this.ok = true
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        this.error = true
+                    })  
             }
         },
         grabar() {
-            let producto = {
-                nombre: this.nombre,
-                precio: this.precio,
-                franquicia: this.franquicia,
-                tamano : this.tamano,
-                material: this.material,
-                altura: this.altura,
-                imagen1: this.imagen1,
-                imagen2: this.imagen2,
-                imagen3: this.imagen3,
-                imagen4: this.imagen4
+            // Valido los campos
+            this.errorNombre = vacio(this.nombre)
+            this.errorPrecio = (this.precio<1)
+            this.errorFranquicia = vacio(this.franquicia)
+            this.errorTamano = vacio(this.tamano)
+            this.errorMaterial = vacio(this.material)
+            this.errorAltura = vacio(this.altura)
+            this.errorImagen1 = vacio(this.imagen1)
+            this.errorImagen2 = vacio(this.imagen2)
+            this.errorImagen3 = vacio(this.imagen3)
+            this.errorImagen4 = vacio(this.imagen4)
+
+            // Veo si todo está bien
+            if (!this.errorNombre && !this.errorPrecio && !this.errorFranquicia &&
+                !this.errorTamano && !this.errorMaterial && !this.errorAltura &&
+                !this.errorImagen1 && !this.errorImagen2 && !this.errorImagen3 && 
+                !this.errorImagen4)
+            {
+                // Asigno el producto nuevo
+                let producto = {
+                    nombre: this.nombre,
+                    precio: this.precio,
+                    franquicia: this.franquicia,
+                    tamano : this.tamano,
+                    material: this.material,
+                    altura: this.altura,
+                    imagen1: this.imagen1,
+                    imagen2: this.imagen2,
+                    imagen3: this.imagen3,
+                    imagen4: this.imagen4
+                }
+                // Preparo el comando
+                var options = {
+                    body: JSON.stringify(producto),
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    redirect: 'follow'
+                }
+                
+                // Envio el comando
+                fetch(this.url, options)
+                    .then(res => {
+                        this.ok = true
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        this.error = true
+                    })
             }
-            var options = {
-                body: JSON.stringify(producto),
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                redirect: 'follow'
-            }
-            
-            fetch(this.url, options)
-                .then(function () {
-                    alert("Producto creado")
-                    window.location.href = "crud.html";
-                })
-                .catch(err => {
-                    console.error(err);
-                    alert("Error al grabar el producto")
-                })
          }
     },
     created() {
--- js/productosCRUDEditar.js
+++ js/productosCRUDEditar.js
@@ -1,11 +1,33 @@
-console.log(location.search) // lee los argumentos pasados a este formulario
-var id = location.search.substr(4)
-console.log(id)
+// ===================================================== //
+// Función: vacio                                        //
+// Descripción: Determina si valor es vacío o no         //
+// Parámetros:                                           //
+//      campo = valor del campo                          //
+// Retorno:                                              //
+//      ret = TRUE, está vacío                           //
+//           FALSE, no está vacío                        //
+// ===================================================== //
+function vacio(campo) {
+    // Saco espacios y me aseguro que tenga valor
+    campo = (campo == undefined) ? "" : campo.trim();
+  
+    return (campo.length === 0);
+}
 
+// Obtengo el ID del producto
+let params = new URLSearchParams(location.search);
+var id = params.get('id')
+
+// Creo la aplicación VUE
 const { createApp } = Vue
 createApp({
     data() {
         return {
+            // Indico error
+            error: false,
+            // Indico operación exitosa
+            ok: false,
+            // Campos del formulario
             id: 0,
             nombre: "",
             precio: 0,
@@ -17,15 +39,28 @@ createApp({
             imagen2: "",
             imagen3: "",
             imagen4: "",
+            // URL del producto a modificar
             url: 'http://grupo18.pythonanywhere.com/productos/' + id,
+            // Error en los campos
+            errorNombre: false,
+            errorPrecio: false,
+            errorFranquicia: false,
+            errorTamano: false,
+            errorMaterial: false,
+            errorAltura: false,
+            errorImagen1: false,
+            errorImagen2: false,
+            errorImagen3: false,
+            errorImagen4: false,
         }
     },
     methods: {
         fetchData(url) {
+            // Obtengo los datos del producto
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
-
+                    // Asigno los datos del producto
                     console.log(data)
                     this.id = data.id
                     this.nombre = data.nombre;
@@ -45,33 +80,55 @@ createApp({
                 })
         },
         modificar() {
-            let producto = {
-                nombre: this.nombre,
-                imagen1: this.imagen1,
-                imagen2: this.imagen2,
-                imagen3: this.imagen3,
-                imagen4: this.imagen4,
-                precio: this.precio,
-                franquicia: this.franquicia,
-                altura: this.altura,
-                tamano: this.tamano,
-                material: this.material
-            }
-            var options = {
-                body: JSON.stringify(producto),
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                redirect: 'follow'
-            }
-            fetch(this.url, options)
-                .then(function () {
-                    alert("Registro modificado")
-                    window.location.href = "crud.html";
-                })
-                .catch(err => {
-                    console.error(err);
-                    alert("Error al Modificar")
-                })
+            // Valido los campos
+            this.errorNombre = vacio(this.nombre)
+            this.errorPrecio = (this.precio<1)
+            this.errorFranquicia = vacio(this.franquicia)
+            this.errorTamano = vacio(this.tamano)
+            this.errorMaterial = vacio(this.material)
+            this.errorAltura = vacio(this.altura)
+            this.errorImagen1 = vacio(this.imagen1)
+            this.errorImagen2 = vacio(this.imagen2)
+            this.errorImagen3 = vacio(this.imagen3)
+            this.errorImagen4 = vacio(this.imagen4)
+
+            // Veo si todo está bien
+            if (!this.errorNombre && !this.errorPrecio && !this.errorFranquicia &&
+                !this.errorTamano && !this.errorMaterial && !this.errorAltura &&
+                !this.errorImagen1 && !this.errorImagen2 && !this.errorImagen3 && 
+                !this.errorImagen4)
+            {
+                // Asigno el producto
+                let producto = {
+                    nombre: this.nombre,
+                    imagen1: this.imagen1,
+                    imagen2: this.imagen2,
+                    imagen3: this.imagen3,
+                    imagen4: this.imagen4,
+                    precio: this.precio,
+                    franquicia: this.franquicia,
+                    altura: this.altura,
+                    tamano: this.tamano,
+                    material: this.material
+                }
+                // Preparo el comando
+                var options = {
+                    body: JSON.stringify(producto),
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    redirect: 'follow'
+                }
+
+                // Envío el comando
+                fetch(this.url, options)
+                    .then(data => {
+                        this.ok = true
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        this.error = true
+                    })
+             }
         }
     },
     created() {
